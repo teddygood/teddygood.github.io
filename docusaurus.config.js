7bf2ac13d16746e8f003599da5cae1ea6d75a168
teddygood <ibear6954@gmail.com>
2022-09-07 20:22:17 +0900

    feat: Google Analytics 4 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-EFT0SBFJCH",
+        },
       }),
     ],
   ],
