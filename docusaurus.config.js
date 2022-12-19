39c7fe7576a35fb4b296ed0085635eb87bcea341
Chanho Lee <ibear6954@gmail.com>
2022-12-19 08:03:42 +0000

    feat: 페이지 편집 기능 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           rehypePlugins: [katex],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/teddygood/teddygood.github.io",
+          editUrl: "https://github.com/teddygood/teddygood.github.io/tree/main",
         },
         blog: {
           showReadingTime: false,
@@ -64,7 +64,7 @@ const config = {
           rehypePlugins: [katex],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/teddygood/teddygood.github.io",
+          editUrl: "https://github.com/teddygood/teddygood.github.io/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
