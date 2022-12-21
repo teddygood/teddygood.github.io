f574b8346e9c0e6d8ec5c6a322aee6ff77141733
Chanho Lee <ibear6954@gmail.com>
2022-12-21 06:20:42 +0000

    fix: 블로그 메인 페이지에 댓글 뜨는 것 수정

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
-import Giscus from '@site/src/components/Giscus'
-
+import Giscus from '@site/src/components/Giscus';
+// @ts-ignore
+import {useBlogPost} from '@docusaurus/theme-common/internal';
+import { useLocation } from '@docusaurus/router';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): JSX.Element {
+  const { metadata } = useBlogPost()
+  const location = useLocation();
+  
+  const { frontMatter, slug, title } = metadata
+  const { comments = true } = frontMatter;
+
+  const { pathname } = location;
+  const showComments = comments && pathname !== "/blog";
+
   return (
     <>
       <BlogPostItem {...props} />
-      <Giscus />
+      {showComments && (
+        <Giscus />
+      )}
     </>
   );
 }
