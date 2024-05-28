0e83fe8857aa8c6115b11cdb9a86c2d453ca0a63
teddygood <ibear6954@gmail.com>
2024-05-29 02:21:40 +0900

    design: remove giscus from blog

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
-import type {WrapperProps} from '@docusaurus/types';
+import type { WrapperProps } from '@docusaurus/types';
 import Giscus from '@site/src/components/Giscus';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): JSX.Element {
+  const url = window.location.href;
+  const [_, detailSlug] = url.split('/blog');
 
   return (
     <>
       <BlogPostItem {...props} />
-      <Giscus />
+      {detailSlug && <Giscus />}
     </>
   );
 }
