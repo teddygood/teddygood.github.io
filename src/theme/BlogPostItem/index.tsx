daaf5ac8fd53e2edb61e62febcf14207527c18fd
Chanho Lee <ibear6954@gmail.com>
2024-05-29 16:45:10 +0900

    refactor: docusaurus/router를 사용하여 빌드 오류 해결 및 재구현

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -3,17 +3,24 @@ import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type { WrapperProps } from '@docusaurus/types';
 import Giscus from '@site/src/components/Giscus';
+import {useLocation} from '@docusaurus/router'
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): JSX.Element {
-  const url = window.location.href;
-  const [_, detailSlug] = url.split('/blog');
+  // const url = window.location.href;
+  // const [_, detailSlug] = url.split('/blog');
+  const location = useLocation();
+
+  // Separate your blog's main page from its subpages
+  const isBlogPage = location.pathname === '/blog';
+  const isBlogPostPage = location.pathname.startsWith('/blog/') && location.pathname.length > '/blog/'.length;
 
   return (
     <>
       <BlogPostItem {...props} />
-      {detailSlug && <Giscus />}
+      {/*{detailSlug && <Giscus />}*/}
+      {isBlogPostPage && <Giscus />}
     </>
   );
 }
