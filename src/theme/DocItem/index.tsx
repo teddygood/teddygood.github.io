f317b1d68dafc32be3433d7287f2733367acfeb8
teddygood <ibear6954@gmail.com>
2024-06-02 23:54:40 +0900

    docs: update wiki

--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import DocItem from '@theme-original/DocItem';
 import type DocItemType from '@theme/DocItem';
 import type {WrapperProps} from '@docusaurus/types';
-import Giscus from '@site/src/components/Giscus';
 
 type Props = WrapperProps<typeof DocItemType>;
 
 export default function DocItemWrapper(props: Props): JSX.Element {
 
-    return (
+  return (
     <>
       <DocItem {...props} />
-      {/* <Giscus /> */}
     </>
   );
 }
---
+++ wiki/Journals/Journals.md
@@ -0,0 +1,9 @@
+---
+title: "Journals"
+date: '2024-06-02'
+draft: false
+
+description: Journals
+---
+
+매일 쓰는 글 중 일부를 공유합니다. 제 생각, 고찰, 인사이트가 이 글을 읽는 당신에게 도움이 되길 바라겠습니다.
