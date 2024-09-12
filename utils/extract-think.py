102dea83baa8acb1f271a8d21c9a65a398abd57d
Chanho Lee <ibear6954@gmail.com>
2024-09-12 13:16:16 +0900

    refactor: remove unnecessary parts

--- utils/extract-think.py
+++ utils/extract-think.py
@@ -15,7 +15,7 @@ def save_to_new_markdown(extracted_text: str, directory: str = 'wiki/Journals') 
     new_file_name = f'{current_date}.md'
     new_file_path = os.path.join(directory, new_file_name)
 
-    metadata = f"""---\ndate: '{current_date}'\nslug: '/{current_date}'\ndescription: ''\n---\n\n"""
+    metadata = f"""---\ndate: '{current_date}'\nslug: '/{current_date}'\n---\n\n"""
 
     with open(new_file_path, 'w', encoding='utf-8') as file:
         file.write(metadata)
